290f38e2f39501b9e56820e79ff5f49c30f61fb4
sokolov uriy <sokolov-uriy@rambler.ru>
2023-01-20 16:37:46 +0300

    coc-errors correction

--- conveyor/src/main/java/shrek/exceptions/BusinessException.java
+++
@@ -1,9 +0,0 @@
-package org.shrek.exceptions;
-
-
-public class BusinessException extends CustomException {
-
-    public BusinessException(int code, String message) {
-        super(code, message);
-    }
-}
