679fbd68363ae0a165b9baf87a2519778a3d51cb
sokolov uriy <sokolov-uriy@rambler.ru>
2023-01-29 17:30:04 +0300

    coc-errors_correction_v4_resolve_structure_ang_test_mistakes

---
+++ conveyor/src/main/java/org/shrek/ConveyorApiApplication.java
@@ -0,0 +1,15 @@
+package org.shrek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ConveyorApiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ConveyorApiApplication.class, args);
+    }
+
+}
